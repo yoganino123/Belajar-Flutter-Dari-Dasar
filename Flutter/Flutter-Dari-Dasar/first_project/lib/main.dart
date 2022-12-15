de5e4376b3aeac8c34c97ba26c7b071f44bdcbd7
yoganino123 <yoganinorahardian@gmail.comm>
2022-12-15 21:29:20 +0700

    Flutter Dasar 2

--- Flutter/Flutter-Dari-Dasar/first_project/lib/main.dart
+++ Flutter/Flutter-Dari-Dasar/first_project/lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(BelajarListView());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          body: Center(
+          body: const Center(
               child: Text(
             "HELLO WORLD!",
             // maxLines: 2,
@@ -31,8 +30,117 @@ class MyApp extends StatelessWidget {
             ),
           )),
           appBar: AppBar(
-            title: Text("Yoganino Apps"),
+            title: const Text("Yoganino Apps"),
           ),
         ));
+  }
+}
+
+// Visible Invisible
+class VisibleInvisble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Coloumn"),
+      ),
+      body: Stack(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 400,
+              width: 400,
+              color: Colors.green,
+            ),
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.blue,
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.amber,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.red,
+            )
+          ]),
+    ));
+  }
+}
+
+// ListView
+class BelajarListView extends StatelessWidget {
+  final List<Color> myColor = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.amber
+  ];
+
+  final List<Widget> myList2 = List.generate(
+    100,
+    (index) => Text(
+      "${index + 1}",
+      style: TextStyle(fontSize: 20 + double.parse(index.toString())),
+    ),
+  );
+
+  final List<Widget> myList = [
+    Container(
+      height: 300,
+      width: 300,
+      color: Colors.red,
+    ),
+    Container(
+      height: 300,
+      width: 300,
+      color: Colors.green,
+    ),
+    Container(
+      height: 300,
+      width: 300,
+      color: Colors.blue,
+    ),
+    Container(
+      height: 300,
+      width: 300,
+      color: Colors.amber,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("List View"),
+        ),
+        body: ListView(
+          children: myList2,
+          // separatorBuilder: (context, index) {
+          //   return Divider(
+          //     color: Colors.black,
+          //   );
+          // },
+          // itemCount: myColor.length,
+          // itemBuilder: (context, index) {
+          //   // return Container(
+          //   //   height: 300,
+          //   //   width: 300,
+          //   //   color: myColor[index],
+          //   // );
+          //   return myList2;
+          // },
+          // scrollDirection: Axis.horizontal,
+          // children: myList
+        ),
+      ),
+    );
   }
 }
